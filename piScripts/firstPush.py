a4c1ba7d64da7b2e41a1c021fceca1e98705c1c5
LucasTyteca <lucas.tyteca@student.howest.be>
2017-06-02 23:04:10 +0200

    first files for the large quiz buttons

---
+++ piScripts/firstPush.py
@@ -0,0 +1,44 @@
+from RPi import GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+#knoppen declareren
+buttons = [4, 17, 27, 22] #oranje, rood, groen, blauw
+buttonOrange = 4
+buttonRed = 17
+buttonGreen = 27
+buttonBlue = 22
+valueReadButtons = [10, 10, 10, 10]
+
+#leds declareren
+leds = [5, 6, 13, 19] #oranje, rood, groen, blauw
+ledOrange = 5
+ledRed = 6
+ledGreen = 19
+ledBlue = 26
+
+#andere variabelen
+notWon = True
+
+#buttons setup
+for x in range(0,4):
+    GPIO.setup(buttons[x], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+#leds setup
+for x in range(0,4):
+    GPIO.setup(leds[x], GPIO.OUT)
+    GPIO.output(leds[x], GPIO.HIGH)
+
+def readButtons():
+    for x in range(0,4):
+        if GPIO.input(buttons[x]) == 0:
+            return x
+    return 9
+
+while notWon == True:
+    pushed = readButtons()
+    if pushed != 9:
+        for x in range(0,4):
+            if x != pushed:
+                GPIO.output(leds[x], GPIO.LOW)
+        notWon = False
---
+++ piScripts/startup.py
@@ -0,0 +1,31 @@
+from RPi import GPIO as GPIO
+import time
+from random import randint
+GPIO.setmode(GPIO.BCM)
+
+#leds declareren
+leds = [5, 6, 13, 19] #oranje, rood, groen, blauw
+
+#andere variabelen
+vorigRandom = 7
+
+#leds setup
+for x in range(0,4):
+    GPIO.setup(leds[x], GPIO.OUT)
+
+try:
+    while True:
+        random = randint(0,3)
+        while random == vorigRandom:
+            random = randint(0,3)
+        GPIO.output(leds[random], GPIO.HIGH)
+        if vorigRandom != 7:
+            GPIO.output(leds[vorigRandom], GPIO.LOW)
+        vorigRandom = random
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    for x in range(0,4):
+        GPIO.output(leds[x], GPIO.LOW)
+
+GPIO.cleanup()
+
